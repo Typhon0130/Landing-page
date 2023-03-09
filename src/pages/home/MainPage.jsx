03e907b2beb9a0eb19d1cbd74081eee2cafa272e
Minato0130 <crystalmaster0130@gmail.com>
2023-03-08 20:39:04 -0800

    Update

--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Layout from '../../components/layout'
 
 export default function MainPage() {
+    const { Row } = Layout;
   return (
     <Layout gridColumnStart='2' gridColumnEnd='7' style = {{border: '1px solid red'}}>
-        Hi
+        <Row backgroundImg>
+
+        </Row>
+        <Row>
+
+        </Row>
     </Layout>
   )
 }
